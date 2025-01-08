ee6b8eab6d54cc57a844f42e7e870a17aa6fdd0c
Manohar-1305 <maddy98989@gmail.com>
2025-01-08 22:42:42 +0530

    Update 6.private-subnet-route.tf

--- terraform/6.private-subnet-route.tf
+++ terraform/6.private-subnet-route.tf
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.dev_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name = "private_route_table"
-  }
-}
 
 resource "aws_route_table_association" "dev_private_route" {
   subnet_id      = aws_subnet.dev_subnet_private_1.id
   route_table_id = aws_route_table.private_route_table.id
 
 }
-=======
+
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.dev_vpc.id
 
@@ -30,9 +18,3 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-resource "aws_route_table_association" "dev_private_route" {
-  subnet_id      = aws_subnet.dev_subnet_private_1.id
-  route_table_id = aws_route_table.private_route_table.id
-
-}
->>>>>>> 8268c6b (added files)
