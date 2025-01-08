f3b0c97be5fb918223d113d334f3ad896e771066
Manohar-1305 <maddy98989@gmail.com>
2025-01-08 22:42:29 +0530

    Update 5.private-subnet.tf

--- terraform/5.private-subnet.tf
+++ terraform/5.private-subnet.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 resource "aws_subnet" "dev_subnet_private_1" {
   vpc_id                  = aws_vpc.dev_vpc.id
   cidr_block              = "10.20.5.0/24"
@@ -6,19 +6,8 @@ resource "aws_subnet" "dev_subnet_private_1" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "dev_subnet_private_1"
-  }
-}
-=======
-resource "aws_subnet" "dev_subnet_private_1" {
-  vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = "10.20.5.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "dev_subnet_private_1"
+    Name                               = "dev_subnet_private_1"
     "kubernetes.io/cluster/kubernetes" = "owned"
   }
 }
->>>>>>> 8268c6b (added files)
+
