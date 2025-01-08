f317e38e4792ca407f770520986eb32d0bebe483
Manohar-1305 <maddy98989@gmail.com>
2025-01-08 22:43:17 +0530

    Update 8.data-iam-role.tf

--- terraform/8.data-iam-role.tf
+++ terraform/8.data-iam-role.tf
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-data "aws_iam_instance_profile" "s3-access-profile" {
-  name = "s3-bucket-access"
-}
-=======
+
 data "aws_iam_instance_profile" "s3-access-profile" {
  # name = "s3-bucket-access"
   name = "s3-access-profile"
 }
->>>>>>> 8268c6b (added files)
+
