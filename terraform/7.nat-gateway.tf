48fd171d628d70e8d82b75bf4d6490dd1c55eb40
Manohar-1305 <maddy98989@gmail.com>
2025-01-08 22:42:55 +0530

    Update 7.nat-gateway.tf

--- terraform/7.nat-gateway.tf
+++ terraform/7.nat-gateway.tf
@@ -1,36 +1,21 @@
-<<<<<<< HEAD
+
 resource "aws_eip" "nat_eip" {
 
 }
-
-resource "aws_nat_gateway" "nat_gateway" {
-  subnet_id     = aws_subnet.dev_subnet_public_1.id
-  allocation_id = aws_eip.nat_eip.id
-  depends_on    = [aws_internet_gateway.dev_public_igw]
-}
-=======
-resource "aws_eip" "nat_eip" {
-  tags = {
-    "Name" = "nat_eip"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-}
-
 resource "aws_nat_gateway" "nat_gateway" {
   subnet_id     = aws_subnet.dev_subnet_public_1.id
   allocation_id = aws_eip.nat_eip.id
   depends_on    = [aws_internet_gateway.dev_public_igw]
 }
 
-resource "aws_eip" "load_balancer_eip" {
+resource "aws_eip" "bastion_eip" {
   tags = {
-    "Name" = "loadbalancer-eip"
-    "kubernetes.io/cluster/kubernetes" = "owned"
+    Name = "bastion_eip"
   }
 }
-resource "aws_eip_association" "lb_eip_assoc" {
-  instance_id   = aws_instance.load-balancer-server.id
-  allocation_id = aws_eip.load_balancer_eip.id
-  depends_on    = [aws_instance.load-balancer-server]
+resource "aws_eip_association" "bastion_eip_assoc" {
+  instance_id   = aws_instance.bastion.id
+  allocation_id = aws_eip.bastion_eip.id
+  depends_on    = [aws_instance.bastion]
+
 }
->>>>>>> 8268c6b (added files)
