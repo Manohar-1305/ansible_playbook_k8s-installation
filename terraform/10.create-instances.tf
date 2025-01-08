e3bce2ff79958c4895f5cc5513a3dc7b988079a8
Manohar-1305 <maddy98989@gmail.com>
2025-01-08 22:44:11 +0530

    Update 10.create-instances.tf

--- terraform/10.create-instances.tf
+++ terraform/10.create-instances.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 resource "aws_instance" "bastion" {
   ami                  = var.instance_ami_type
   instance_type        = var.instance_type_bastion
@@ -6,11 +6,8 @@ resource "aws_instance" "bastion" {
   subnet_id            = aws_subnet.dev_subnet_public_1.id
   iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.nat_gateway_sg.id,
+    aws_security_group.combined_sg.id,
+    aws_security_group.haproxy_sg.id,
   ]
   user_data = file("user-script-bastion.sh")
 
@@ -36,11 +33,8 @@ resource "aws_instance" "nfs_server" {
   subnet_id            = aws_subnet.dev_subnet_public_1.id
   iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.Web-traffic.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.nfs.id
+    aws_security_group.combined_sg.id,
+    aws_security_group.haproxy_sg.id,
   ]
   user_data = file("user-script-nfs.sh")
   tags = {
@@ -51,22 +45,19 @@ resource "aws_instance" "nfs_server" {
   depends_on = [null_resource.pause_before_nfs]
 }
 
-resource "aws_instance" "load-balancer-server" {
+resource "aws_instance" "master" {
   ami                  = var.instance_ami_type
   instance_type        = var.instance_type_lb
   key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_public_1.id
+  subnet_id            = aws_subnet.dev_subnet_private_1.id
   iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.haproxy-sg.id,
+    aws_security_group.combined_sg.id,
+    aws_security_group.haproxy_sg.id,
   ]
   user_data = file("user-script-lb.sh")
   tags = {
-    "Name"                             = "loadbalancer"
+    "Name"                             = "master1"
     "Enviroment"                       = "Development"
     "kubernetes.io/cluster/kubernetes" = "owned"
   }
@@ -76,7 +67,7 @@ resource "null_resource" "pause_before_master_nodes" {
   provisioner "local-exec" {
     command = "sleep 120"
   }
-  depends_on = [aws_instance.bastion, aws_instance.load-balancer-server]
+  depends_on = [aws_instance.bastion, aws_instance.master]
 }
 
 resource "aws_instance" "master-server" {
@@ -87,15 +78,12 @@ resource "aws_instance" "master-server" {
   subnet_id            = aws_subnet.dev_subnet_private_1.id
   iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.haproxy-sg.id,
+    aws_security_group.combined_sg.id,
+    aws_security_group.haproxy_sg.id,
   ]
   user_data = file("user-script-node.sh")
   tags = {
-    "Name"                             = "master${count.index + 1}"
+    "Name"                             = "master${count.index + 2}"
     "Enviroment"                       = "Development"
     "kubernetes.io/cluster/kubernetes" = "owned"
   }
@@ -118,145 +106,8 @@ resource "aws_instance" "worker-server" {
   subnet_id            = aws_subnet.dev_subnet_private_1.id
   iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.haproxy-sg.id,
-  ]
-  user_data = file("user-script-node.sh")
-  tags = {
-    "Name"                             = "worker${count.index + 1}"
-    "Enviroment"                       = "Development"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-}
-=======
-resource "aws_instance" "bastion" {
-  ami                  = var.instance_ami_type
-  instance_type        = var.instance_type_bastion
-  key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_public_1.id
-  iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.Web-traffic.id
-  ]
-  user_data = file("user-script-bastion.sh")
-
-  tags = {
-    "Name"                             = "bastion"
-    "Enviroment"                       = "Development"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-}
-
-resource "null_resource" "pause_before_nfs" {
-  provisioner "local-exec" {
-    command = "sleep 90"
-  }
-  depends_on = [aws_instance.bastion]
-}
-
-
-resource "aws_instance" "nfs_server" {
-  ami                  = var.instance_ami_type
-  instance_type        = var.instance_type_nfs
-  key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_public_1.id
-  iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.Web-traffic.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.nfs.id
-  ]
-  user_data = file("user-script-nfs.sh")
-  tags = {
-    "Name"                             = "nfs"
-    "Enviroment"                       = "Development"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-  depends_on = [null_resource.pause_before_nfs]
-}
-
-resource "aws_instance" "load-balancer-server" {
-  ami                  = var.instance_ami_type
-  instance_type        = var.instance_type_lb
-  key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_public_1.id
-  iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.haproxy-sg.id,
-  ]
-  user_data = file("user-script-lb.sh")
-  tags = {
-    "Name"                             = "loadbalancer"
-    "Enviroment"                       = "Development"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-}
-
-resource "null_resource" "pause_before_master_nodes" {
-  provisioner "local-exec" {
-    command = "sleep 120"
-  }
-  depends_on = [aws_instance.bastion, aws_instance.load-balancer-server]
-}
-
-resource "aws_instance" "master-server" {
-  count                = var.master_instance_count
-  ami                  = var.instance_ami_type
-  instance_type        = var.instance_type_master
-  key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_private_1.id
-  iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.Web-traffic.id,
-  ]
-  user_data = file("user-script-node.sh")
-  tags = {
-    "Name"                             = "master${count.index + 1}"
-    "Enviroment"                       = "Development"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-  depends_on = [null_resource.pause_before_master_nodes]
-}
-
-resource "null_resource" "pause_before_worker_nodes" {
-  provisioner "local-exec" {
-    command = "sleep 90"
-  }
-  depends_on = [aws_instance.master-server]
-}
-
-resource "aws_instance" "worker-server" {
-  depends_on           = [null_resource.pause_before_worker_nodes]
-  count                = var.worker_instance_count
-  ami                  = var.instance_ami_type
-  instance_type        = var.instance_type_worker
-  key_name             = "testing-dev-1"
-  subnet_id            = aws_subnet.dev_subnet_private_1.id
-  iam_instance_profile = data.aws_iam_instance_profile.s3-access-profile.name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.kubernetes.id,
-    aws_security_group.nat_gateway_sg.id,
-    aws_security_group.open_access_within_vpc.id,
-    aws_security_group.haproxy-sg.id,
+    aws_security_group.combined_sg.id,
+    aws_security_group.haproxy_sg.id,
   ]
   user_data = file("user-script-node.sh")
   tags = {
@@ -266,4 +117,3 @@ resource "aws_instance" "worker-server" {
   }
 }
 
->>>>>>> 8268c6b (added files)
