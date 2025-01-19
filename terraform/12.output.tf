424a39e4301596ef816e05f29098b7494a9f795a
Manohar-1305 <maddy98989@gmail.com>
2025-01-19 23:00:14 +0530

    Update 12.output.tf

--- terraform/12.output.tf
+++ terraform/12.output.tf
@@ -1,6 +1,12 @@
-
 output "bastion_public_ip" {
   description = "The Bastion Instance Public IP"
   value       = aws_instance.bastion.public_ip
 }
-
+output "NFS_public_ip" {
+  description = "The Load Balancer Instance Public IP"
+  value       = aws_instance.nfs_server.public_ip
+}
+output "loadbalancer_public_ip" {
+  description = "The Load Balancer Instance Public IP"
+  value       = aws_instance.load-balancer-server.public_ip
+}
